9176aad56b6ea578841023b464f3654a65c6085c
“testhuang <tes.mail>
2020-09-22 09:59:35 +0800

    update dao to mapper

--- src/main/java/com/ha5fun/finalwork/mapper/RpmsBuildMapper.java
+++ src/main/java/com/ha5fun/finalwork/mapper/RpmsBuildMapper.java
@@ -5,12 +5,19 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-/**
- * @author chen
- * @Company ha5fun.com
- * @Date 2020/9/21 8:31 下午
- */
 @Mapper
 public interface RpmsBuildMapper {
-    List<RpmsBuildPojo> selAllBuild() ;
+
+
+    int deleteByPrimaryKey(Integer buildId);
+
+    int insert(RpmsBuildPojo record);
+
+    int insertSelective(RpmsBuildPojo record);
+
+    RpmsBuildPojo selectByPrimaryKey(Integer buildId);
+
+    int updateByPrimaryKeySelective(RpmsBuildPojo record);
+
+    int updateByPrimaryKey(RpmsBuildPojo record);
 }
